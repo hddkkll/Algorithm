51406d2d697f86ad4e4410a2cf37f36306f504ca
abab <ababc@ab.com>
2019-03-21 17:20:28 +0900

    commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# Algorithm
-bubble_sort : https://github.com/hddkkll/Algorithm/issues/1#issue-423128866
--- src/sort/Bubble.java
+++ src/sort/Bubble.java
@@ -7,7 +7,7 @@ public class Bubble {
 	public static void main(String[] args) {
 		int arr[]= {5,4,1,3,2,6};
 		int temp;
-		
+
 		//처음값출력
 		System.out.print("정렬전 : ");
 		for(int i = 0 ; i<arr.length;i++)
